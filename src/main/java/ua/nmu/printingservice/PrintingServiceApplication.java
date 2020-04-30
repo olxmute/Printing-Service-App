bbcab778958a82d08687088bc8d5ca03d0a09128
Oleksii Mutianov <mutianov.o.o@gmail.com>
2020-04-30 16:14:32 +0300

    Initial commit

---
+++ src/main/java/ua/nmu/printingservice/PrintingServiceApplication.java
@@ -0,0 +1,13 @@
+package ua.nmu.printingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrintingServiceApplication.class, args);
+	}
+
+}
