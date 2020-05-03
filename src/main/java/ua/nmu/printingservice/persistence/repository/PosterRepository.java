6aa9b67dc1992d48e826000a02e5c8f91de3a836
Oleksii Mutianov <38989947+oleksii-mutianov@users.noreply.github.com>
2020-05-03 20:48:08 +0300

    Make changing 'active' flag to 'false' on poster deleting instead of actual deleting (#9)

--- src/main/java/ua/nmu/printingservice/persistence/repository/PosterRepository.java
+++ src/main/java/ua/nmu/printingservice/persistence/repository/PosterRepository.java
@@ -2,10 +2,19 @@ package ua.nmu.printingservice.persistence.repository;
 
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ua.nmu.printingservice.persistence.domain.product.Poster;
 
 import java.util.List;
 
 public interface PosterRepository extends JpaRepository<Poster, String> {
-    List<Poster> findAllByUserNull(Sort sort);
+
+    List<Poster> findAllByActiveAndUserNull(boolean active, Sort sort);
+
+    @Modifying
+    @Query("UPDATE Poster p SET p.active = false WHERE p = :poster")
+    void delete(@Param("poster") Poster poster);
+
 }
