3f4931f86dc93be5ec86b73e5edf73e8383558e6
Oleksii Mutianov <mutianov.o.o@gmail.com>
2020-05-10 14:40:07 +0300

    Remove unused CSS styles

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -27,23 +27,6 @@
     });
 
     /*------------------
-        Hero Slider
-    --------------------*/
-    $(".hero-items").owlCarousel({
-        loop: true,
-        margin: 0,
-        nav: true,
-        items: 1,
-        dots: false,
-        animateOut: 'fadeOut',
-        animateIn: 'fadeIn',
-        navText: ['<i class="ti-angle-left"></i>', '<i class="ti-angle-right"></i>'],
-        smartSpeed: 1200,
-        autoHeight: false,
-        autoplay: true,
-    });
-
-    /*------------------
         Product Slider
     --------------------*/
    $(".product-slider").owlCarousel({
